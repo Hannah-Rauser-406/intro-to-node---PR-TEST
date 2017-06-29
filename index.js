58f467c99d1c7bedbf7d1961598849e45b19a0bf
Hannah Hendrickson <hannahhendrickson@Hannahs-MBP.PK5001Z>
2017-06-29 11:13:11 -0600

    Added date creator

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var makeDate = require('./dateMaker');
+//if you create your own module(dateMaker)-- you have to be path specific
 var app = express();
+
+console.log(makeDate(10));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -11,5 +15,9 @@ app.get("/instagrams", function(req,res){
 app.post("/instagrams", function(req,res){
   res.send("HEllo from host");
 });
+
+
+
+
 
 var server = app.listen(3000, function(){console.log("Listening on port 3000")});
